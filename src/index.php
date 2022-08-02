f06a976d4bd4a2dbc2a988aebbd5bdc226547a0e
Ernesto Hinojosa <59732463+NestoJosa@users.noreply.github.com>
2022-08-02 23:49:18 +0200

    Create prot

--- src/index.php
+++ src/index.php
@@ -11,20 +11,34 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- Load our styles bundle -->
-    <link rel="stylesheet" href="dist/css/bundle.css">
+    <link rel="stylesheet" href="./css/bundle.css">
 
 </head>
 <body>
 
-    <div class="container-fluid">
+    <?php 
+    
+        $isMirroredOnHorizontalLayout = false;
+
+        $bootstrapOrderClass = $isMirroredOnHorizontalLayout ? 'order-md-2' : '';
+    
+    ?>
+
+    <div class="Hero container-fluid">
         <div class="container">
             <div class="row">
-                <div class="col">
-                    <h1>Hello there</h1>
+
+                <div class="Hero__image col-md-6 <?= $bootstrapOrderClass ?>">
+                    Some Image
                 </div>
-            </div>
-        </div>
-    </div>
+
+                <div class="Hero__text col-md-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+
+            </div><!-- /row -->
+        </div><!-- /container -->
+    </div><!-- /container-fluid -->
 
     <footer>
 
